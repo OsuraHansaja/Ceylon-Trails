faccfcb404faef79a1bf15d5f8fa63ef878ea712
Osura De Silva <osuradesilva@hotmail.com>
2024-07-23 19:16:01 +0530

    New Database Migrations

---
+++ database/migrations/2024_07_23_134056_add_custom_fields_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('username')->unique()->nullable();
+            $table->date('dob')->nullable();
+            $table->enum('gender', ['Female', 'Male', 'Custom'])->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['first_name', 'last_name', 'username', 'dob', 'gender']);
+        });
+    }
+
+};
