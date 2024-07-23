b687b4a0c4e3f101d880653dd66b2dc799a6900b
Osura De Silva <osuradesilva@hotmail.com>
2024-07-23 18:39:51 +0530

    Made Routing changes to utilize new Home Page

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return view('home');
 });
+
 
 Route::middleware([
     'auth:sanctum',
