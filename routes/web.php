f443f6c380b8656fc3cc8d7778cc385633ba3abb
Osura De Silva <osuradesilva@hotmail.com>
2024-09-12 07:50:03 +0530

    Fixed filters

--- routes/web.php
+++ routes/web.php
@@ -190,4 +190,15 @@ use App\Http\Controllers\ReviewController;
 Route::post('/item/{item}/review', [ReviewController::class, 'store'])->name('reviews.store');
 Route::delete('/item/{item}/review/{review}', [ReviewController::class, 'destroy'])->name('reviews.destroy');
 
+// Route to filter items based on category
+Route::get('/filter-items/{categoryId}', [ItemController::class, 'filterItems'])->name('filter.items');
+
+// Route to filter events based on category
+Route::get('/filter-events/{categoryId}', [EventController::class, 'filterEvents'])->name('filter.events');
+
+// Route to handle filter and pagination for items (attractions)
+Route::get('/filter-items-paginated', [ItemController::class, 'filterItemsPaginated'])->name('filter.items.paginated');
+
+//Route to handle filtering in happenings
+Route::get('/filter-happenings-paginated', [EventController::class, 'filterHappeningsPaginated'])->name('filter.happenings.paginated');
 
